52a615940fdc16ca3d8da0bf02cdb9a2159809f4
MintedKitten <87274176+MintedKitten@users.noreply.github.com>
2022-11-26 11:23:07 +0700

    Workshop A

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  return res.status(200).json({ fullname: "Korndanai" });
+});
+
+router.get("/bio", (req, res, next) => {
+  return res
+    .status(200)
+    .json({
+      fullname: "Korndanai Ananjinda",
+      nickname: "Sky",
+      hobby: "Listening to Music",
+      gitusername: "MintedKitten",
+    });
 });
 
 module.exports = router;
